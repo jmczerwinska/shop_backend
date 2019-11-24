99d1118c6321ccb0cb4039e34c413f38d51aec68
jmczerwinska <asia.frej@gmail.com>
2019-11-24 14:23:32 +0100

    add check password

--- controlers/auth-controller.js
+++ controlers/auth-controller.js
@@ -1,4 +1,5 @@
 const PouchDB = require('pouchdb');
+PouchDB.plugin(require('pouchdb-find'));
 
 class AuthController {
     constructor(){
@@ -6,7 +7,9 @@ class AuthController {
 
     }
 
-    checkPassword(){}
+    changePassword(id,data){
+        return this._getUser(id).then(user => this._updateUser(user,data));
+    }
 
     createUser(login, password, email){
         return this.db.post({
@@ -16,7 +19,20 @@ class AuthController {
         });
     }
     
-    changePassword(){}
+    checkPassword(login, password){
+        return this.db.find({
+            selector: {login ,password},
+            fields: ['_id','login']
+        });
+    }
+
+    _getUser(id) {
+        return this.db.get(id);
+    }
+
+    _updateUser(user, data){
+        return this.db.put({...user, ...data})
+    }
 }
 
 module.exports = AuthController;
--- router/auth.js
+++ router/auth.js
@@ -13,27 +13,43 @@ class AuthRouter {
     routes(){
         // POST api/auth/
         this.router.post('/', jsonParser, this._checkPassword.bind(this));
+        // POST api/auth/create
         this.router.post('/create', jsonParser, this._createUser.bind(this));
-        // PUT api/auth
-        this.router.put('/', jsonParser, this._changePassword.bind(this));
+        // PUT api/auth/:id
+        this.router.put('/:id', jsonParser, this._changePassword.bind(this));
     }
 
     _checkPassword(req, res) {
-        this.controller.checkPassword()
+        const userData = req.body;
+        this.controller.checkPassword(userData.login, userData.password)
+            .then(response => {
+                if (response.docs.length ===0) {
+                    res.status(401).send('Wrong credentials');
+                } else {
+                    res.status(200).send(response.docs[0])
+                }
+        })
+            
     }
 
     _createUser(req, res) {
         const  userData = req.body;
         this.controller.createUser(userData.login, userData.password, userData.email)
-        .then(respons => {
-            res.send(respons)
-        })
-        .catch((err) => {
-            res.status(500).send(err);
-        });
+            .then(response => {
+                res.send(response)
+            })
+            .catch((err) => {
+                res.status(500).send(err);
+             });
     }
     
-    _changePassword(req, res){}
+    _changePassword(req,res){
+        const id = req.params.id;
+        this.controller.changePassword(id, req.body)
+            .then(response => res.send(response))
+            .catch(err => res.status(500).send(err));
+    }
+
 
 }
 module.exports = AuthRouter;
